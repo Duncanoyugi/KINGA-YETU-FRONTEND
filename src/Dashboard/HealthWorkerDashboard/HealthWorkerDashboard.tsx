f27f1e4354916c3ce8b9d525791b876c938ef1fc
Duncanoyugi <duncanoyugi2001@gmail.com>
2026-04-06 06:21:32 +0300

    change path

--- src/Dashboard/HealthWorkerDashboard/HealthWorkerDashboard.tsx
+++ src/Dashboard/HealthWorkerDashboard/HealthWorkerDashboard.tsx
@@ -1370,7 +1370,7 @@ const HealthWorkerDashboard: React.FC = () => {
           {/* Health worker sub-pages (keep shell) */}
           <Route path="appointments" element={<Appointments />} />
           <Route path="vaccinations/*" element={
-            user?.healthWorker?.facility || user?.healthWorker?.facilityId ? (
+            user?.healthWorker?.facility || user?.healthWorker?.facilityId || localStorage.getItem('facilitySetupCompleted') === 'true' ? (
               <Routes>
                 <Route path="/" element={<VaccinationAdministrationPage />} />
                 <Route path="record/:appointmentId" element={<RecordVaccinationPage />} />
